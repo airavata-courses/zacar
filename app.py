ff4917cb5d2c307027b665afb3d42013cab57ea6
Edsel Brian Norwood II <edselnorwood83@gmail.com>
2019-06-22 14:11:28 -0400

    Change root directory in app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template
-import sys
-sys.path.append("..")
-import modules.get_track
+from modules import get_track
 app = Flask(__name__, template_folder='templates')
  
 @app.route("/")
